49115e170219d6484c390eb68ee0770795d87305
KrystianKaminski <krystiankaminski07@gmail.com>
2019-04-14 18:53:59 +0200

    Countdown functionality

--- pomodoro-app/src/Pomodoro.js
+++ pomodoro-app/src/Pomodoro.js
@@ -3,11 +3,32 @@ import './Pomodoro.css'
 
 class Pomodoro extends Component {
     state = {
-        pomodoroTime: null,
+        pomodoroTime: 20,
+        remainingTime: 0,
         donePomodoros: 0,
         breakTime: null,
         dailyGoal: null,
         ticking: false
+    }
+
+    componentDidMount() {
+        this.setState({
+            remainingTime: this.state.pomodoroTime * 60
+        })
+    }
+
+    startCountdown = () => {
+        this.intervalId = setInterval(this.tick, 1000)
+        this.setState({
+            remainingTime: this.state.remainingTime - 1,
+        })
+
+    }
+
+    tick = () => {
+        this.setState({
+            remainingTime: this.state.remainingTime - 1
+        })
     }
 
     render() {
@@ -17,9 +38,11 @@ class Pomodoro extends Component {
             >
                 <h1>Pomodoro App</h1>
                 <div>
-                    <span>{this.state.pomodoroTime}</span>
+                    <span>{this.state.remainingTime}</span>
                     <span>{this.state.donePomodoros}</span>
-                    <button>Start</button>
+                    <button
+                        onClick={this.startCountdown}
+                    >Start</button>
                 </div>
             </div>
         )
