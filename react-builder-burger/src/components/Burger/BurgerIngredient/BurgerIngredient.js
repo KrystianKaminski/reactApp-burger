30265275397fe69c0ab1df1622cdd522cf386e93
KrystianKaminski <krystiankaminski07@gmail.com>
2019-08-21 19:59:07 +0200

    Prop-type validation for BurgerIngredient

--- react-builder-burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ react-builder-burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import classes from './BurgerIngredient.module.css'
 
@@ -32,6 +33,10 @@ const BurgerIngredient = (props) => {
     }
 
     return ingredient
+}
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient
