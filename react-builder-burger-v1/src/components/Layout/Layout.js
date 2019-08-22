6077a4d830f769035773a57b465b130fa86bc50a
KrystianKaminski <krystiankaminski07@gmail.com>
2019-08-22 20:07:01 +0200

    Adding a Toolbar component

--- react-builder-burger-v1/src/components/Layout/Layout.js
+++ react-builder-burger-v1/src/components/Layout/Layout.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const Layout = (props) => (
     <Auxiliary>
-        <div>
-            Toolbar, SideDrawer, Backdrop
-    </div>
+        <Toolbar />
         <main className={classes.Content}>
             {
                 props.children
