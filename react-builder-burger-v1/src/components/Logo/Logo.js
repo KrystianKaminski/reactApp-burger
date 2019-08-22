dbab58eed38b40f8e67548fc50e23896c26c9c8a
KrystianKaminski <krystiankaminski07@gmail.com>
2019-08-22 20:59:52 +0200

    Responsive SideDrawer component

--- react-builder-burger-v1/src/components/Logo/Logo.js
+++ react-builder-burger-v1/src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import BurgerLogo from '../../assets/images/burger-logo (1).png'
 import classes from './Logo.module.css'
 
 const Logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{ height: props.height }}>
         <img src={BurgerLogo} alt="MyBurger" />
     </div>
 )
