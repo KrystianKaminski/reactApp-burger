6a6869f23d06bc85f77f9d4741f060531d2b1b30
KrystianKaminski <krystiankaminski07@gmail.com>
2019-08-26 12:35:00 +0200

    Improving app performance by prevent unnecessary re-rendering

--- react-builder-burger-v1/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-builder-burger-v1/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,43 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
-import Button from '../../UI/Button/Button'
+import Button from "../../UI/Button/Button";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+
+  componentDidUpdate() {
+    console.log('[OrderSummary] will update')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li
-        key={igKey}
-      >
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A deliciuos burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Auxiliary>
     );
-  });
-
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A deliciuos burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCancelled}
-      >
-        CANCEL
-        </Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinued}
-      >
-        CONTINUE
-        </Button>
-    </Auxiliary>
-  );
-};
+  }
+}
 
 export default OrderSummary;
