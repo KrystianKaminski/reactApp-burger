83a3718c39ed953f04e3f5de6806035f6ac9c400
KrystianKaminski <krystiankaminski07@gmail.com>
2019-08-29 15:28:15 +0200

    Navigating back & to the next page

--- react-builder-burger-v1/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ react-builder-burger-v1/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -18,13 +18,13 @@ const CheckoutSummary = (props) => {
             </div>
             <Button 
                 btnType="Danger"
-                clicked
+                clicked={props.checkoutCancelled}
             >
                 CANCEL
             </Button>
             <Button 
                 btnType="Success"
-                clicked
+                clicked={props.checkoutContinued}
             >
                 CONTINUE
             </Button>
