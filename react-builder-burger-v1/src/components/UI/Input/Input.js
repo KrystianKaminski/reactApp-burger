509b7bc5428cdc63d7ce527f1430352f7d4cfc48
KrystianKaminski <krystiankaminski07@gmail.com>
2019-09-04 13:15:53 +0200

    Handling user input

--- react-builder-burger-v1/src/components/UI/Input/Input.js
+++ react-builder-burger-v1/src/components/UI/Input/Input.js
@@ -1,52 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Input.module.css'
+import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = props => {
+  let inputElement = null;
 
-    let inputElement = null
-    
-    switch (props.elementType) {
-        case ('input'):
-            inputElement = <input
-                className={classes.InputElement}
-                {...props.elementConfig}
-                value={props.value}
-            />
-            break;
-        case ('textarea'):
-            inputElement = <textarea
-                className={classes.InputElement}
-                {...props.elementConfig}
-                value={props.value}
-            />
-            break;
-        case ('select'):
-            inputElement = <select
-                className={classes.InputElement}
-                {...props.elementConfig}
-                value={props.value}
-            >   
-                {props.elementConfig.options.map(option => (
-                    <option key={option.value} value={option.value}>{option.displayValue}</option>
-                ))}
-            </select>
-            break;
-        default:
-            inputElement = <input 
-                className={classes.InputElement}
-                {...props.elementConfig}
-                value={props.value}
-            />
-    }
-    console.log(inputElement)
-    return (
-        <div className={classes.Input}>
-            <label className={classes.Label}>{props.label}</label>
-            {inputElement}
-        </div>
-    )
-}
+  switch (props.elementType) {
+    case "input":
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+  }
+  console.log(inputElement);
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
 
-
-export default Input
+export default Input;
