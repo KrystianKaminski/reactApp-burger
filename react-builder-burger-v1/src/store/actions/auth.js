f20bb50d01c5cd043a4fd01b53dfcaa975ff79b5
KrystianKaminski <krystiankaminski07@gmail.com>
2019-09-11 12:50:12 +0200

    Getting a token from backend

--- react-builder-burger-v1/src/store/actions/auth.js
+++ react-builder-burger-v1/src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import * as actionTypes from './actionTypes'
 
 export const authStart = () => {
@@ -24,5 +26,19 @@ export const auth = (email, password) => {
     return dispatch => {
         // ...
         dispatch(authStart())
-    }
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD9Sy8YL4cXT5KV70qrkMy701dsDigBbZA', authData)
+            .then(response => {
+                console.log(response)
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch(authFail(err))
+            })
+        }
 }
